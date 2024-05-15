62da27aca77bc4c70e58065cf615394ebd1bb56d
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2024-05-15 15:04:07 -0600

    added password example

---
+++ diary/entries/2024/5.15/entry.php
@@ -0,0 +1,6 @@
+hey if youre having troble with your link linky links
+<a href="Gaben.png" >regular link (leads to filebrowser)</a>
+<!--<a href="./Gaben.png" >Direct link (but with filepath included)</a>
+feature not implemented to github release yet
+-->
+<a href='?entry=4.16.2024'>direct link</a>
